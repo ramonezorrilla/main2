a4b71beea4aec387f6b84489cffa843271eaf795
ramonezorrilla <ramoenzorrilla@gmail.com>
2025-01-18 19:55:08 -0400

    Add project files.

---
+++ main2/main2.py
@@ -0,0 +1,5 @@
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("The result of adding 2 and 3 is:", add(2, 3))
---
+++ main2/test_main2.py
@@ -0,0 +1,15 @@
+import unittest
+from main import add
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_add_zero(self):
+        self.assertEqual(add(0, 5), 5)
+
+if __name__ == "__main__":
+    unittest.main()
